a2e82f58eaaa0debeb74b3fc1d65adc843c77fcd
Sarbjeet Kaur <104292115+Sarbjeet28@users.noreply.github.com>
2024-03-07 12:05:04 +0000

    first commit

---
+++ Day2/day2.js
@@ -0,0 +1,8 @@
+const accountid= 1234;
+let accountname= "Sarbjeet kaur";
+var accountcity= "Ludhiana";
+
+accountname= "jagdeep";
+accountcity= "raikot";
+
+console.table([accountid, accountname, accountcity]);
---
+++ Day3/day3.js
@@ -0,0 +1,18 @@
+let number1= 1234;
+let number2= 12345;
+let number3= 12346;
+let number4= 12347;
+let number5= 12348;
+
+let string1= String(number1);
+let string2= String(number2);
+let string3= String(number3);
+let string4= String(number4);
+let string5= String(number5);
+
+console.log(string1);
+console.log(typeof string1);
+console.log(typeof number1);
+console.table([string1, string2, string3, string4, string5]);
+console.log(typeof string2);
+console.log(typeof string3);
