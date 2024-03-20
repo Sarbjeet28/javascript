993d015ae0417048a037432fa0df19414434e549
Sarbjeet Kaur <104292115+Sarbjeet28@users.noreply.github.com>
2024-03-20 16:56:31 +0000

    update day8

--- day8/day8.js
+++ day8/day8.js
@@ -4,5 +4,17 @@ let myarr= [1,2,3,4,5,6,7];
 // myarr.push(8);
 // console.log(myarr);
 
-myarr.pop(8);
-console.log(myarr);
+// myarr.pop();
+// console.log(myarr);
+
+// myarr.shift();
+// console.log(myarr);
+
+// myarr.unshift(0);
+// console.log(myarr);
+
+console.log(myarr.includes(2));
+console.log(myarr.indexOf(2));
+console.log(myarr.join());
+console.log(myarr.slice(0,2));
+console.log(myarr.splice(0,3));
