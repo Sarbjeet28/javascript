24f1b35e1e4d9403a353356f9a414c115619dae6
Sarbjeet Kaur <104292115+Sarbjeet28@users.noreply.github.com>
2024-03-21 11:58:40 +0000

    day8

--- day8/day8.js
+++ day8/day8.js
@@ -16,5 +16,5 @@ let myarr= [1,2,3,4,5,6,7];
 console.log(myarr.includes(2));
 console.log(myarr.indexOf(2));
 console.log(myarr.join());
-console.log(myarr.slice(0,2));
+console.log(myarr.slice(0,3));
 console.log(myarr.splice(0,3));
