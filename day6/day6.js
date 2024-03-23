82ec571394d9d737a9095ca084922560d9b4d3c2
Sarbjeet Kaur <104292115+Sarbjeet28@users.noreply.github.com>
2024-03-23 12:03:49 +0000

    day6

---
+++ day6/day6.js
@@ -0,0 +1,46 @@
+// Strings
+
+const name = "sarb";
+const age = 22;
+// console.log(name+ age);
+// console.log(`my name is ${name} and my age is ${age}`);
+
+let fname= new String("sarbjeet")
+// console.log(fname);
+// console.log(fname.length);
+// console.log(fname.toUpperCase());
+// console.log(fname.toLowerCase());
+// console.log(fname.charAt(2));
+// console.log(fname.indexOf("s"));
+// console.log(fname.substring(0,4));
+// console.log(fname.slice(-8,4));
+
+const nameone ="   sarb   ";
+// console.log(nameone.trim());
+
+const url = "https://www.jdwebservices.com/app%20development.php";
+// console.log(url.replace("%20", "-"));
+// console.log(url.includes("jass"));
+
+const stringone = "Hello-World-India";
+// console.log(stringone.split("-"));
+
+// Numbers
+
+const balance = new Number(100);
+// console.log(balance);
+// console.log(balance.toString());
+// console.log(balance.toFixed(3));
+
+const valuetwo = 23.8995;
+// console.log(valuetwo.toPrecision(3));
+
+const number = 1000000;
+console.log(number.toLocaleString()); // USA Standards
+console.log(number.toLocaleString('en-IN')); // India Standards
+
+
+
+
+
+
