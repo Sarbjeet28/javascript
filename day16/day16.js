dc80fca5fa197f355ddd80f68309f707b0a6caf2
Sarbjeet Kaur <104292115+Sarbjeet28@users.noreply.github.com>
2024-04-28 17:43:52 +0000

    COMMIT

---
+++ day16/day16.js
@@ -0,0 +1,33 @@
+// arrow function
+let a = () => {
+    console.log("hello");
+}
+a();
+
+// if statement
+if(2==2){
+    // console.log("both values are equal");
+}
+
+// if-else
+// if(2===3){
+//     console.log("both values are equal");
+// }
+// else{
+//     console.log("both values are not equal");
+// }
+
+// multiple if statement
+let num= 1000
+if(num<500){
+    console.log("less than 500");
+}
+else if(num<750){
+    console.log("less than 750");
+}
+else if(num<950){
+    console.log("less than 950");
+}
+else{
+    console.log("less than 1200");
+}
